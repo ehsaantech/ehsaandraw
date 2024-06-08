0e9915146765a8acd8ca8313cb220d12b572c263
zahidkhan-xen <zk8127194@gmail.com>
2024-06-08 17:43:53 +0500

    sharable button working

--- src/component/EhsaanDraw/EhsaanDraw.js
+++ src/component/EhsaanDraw/EhsaanDraw.js
@@ -2,27 +2,16 @@ import { Excalidraw } from "@excalidraw/excalidraw";
 import React, { useEffect, useState } from "react";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { useNavigate } from "react-router-dom";
-import { useGithub } from "../../context";
-import toast from "react-hot-toast";
 import ShareIcon from "@mui/icons-material/Share";
 
-function EhsaanDrawScreen({ updateData, scenes }) {
+function EhsaanDrawScreen({
+  updateData,
+  scenes,
+  shareScenesData,
+  readOnly = false,
+}) {
   const [excalidrawAPI, setExcalidrawAPI] = useState(null);
   const navigate = useNavigate();
-  const { githubId } = useGithub();
-  const shareLink = () => {
-    if (githubId) {
-      const url = window.location.href.split("?")[0]; // Remove query parameters
-      navigator.clipboard
-        .writeText(url)
-        .then(() => {
-          toast.success("Link copied to share!");
-        })
-        .catch((error) => {
-          toast.error("Error copying link:", error);
-        });
-    }
-  };
 
   useEffect(() => {
     excalidrawAPI?.updateScene({ elements: scenes });
@@ -40,50 +29,73 @@ function EhsaanDrawScreen({ updateData, scenes }) {
           renderTopRightUI={() => {
             return (
               <>
-                <button
-                  style={{
-                    padding: "2px",
-                    borderRadius: "5px",
-                    background: "#70b1ec",
-                    border: "none",
-                    color: "#fff",
-                    width: "45px",
-                    fontWeight: "bold",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                  onClick={() => navigate("/")}
-                >
-                  <ArrowBackIcon />
-                </button>
-                <button
-                  style={{
-                    background: "#70b1ec",
-                    border: "none",
-                    width: "45px",
-                    color: "#fff",
-                    fontWeight: "bold",
-                    borderRadius: "5px",
-                    padding: "5px",
-                  }}
-                  onClick={shareLink}
-                >
-                  <ShareIcon />
-                </button>
-                <button
-                  style={{
-                    background: "#70b1ec",
-                    border: "none",
-                    color: "#fff",
-                    width: "max-content",
-                    fontWeight: "bold",
-                    borderRadius: "5px",
-                  }}
-                  onClick={() => updateData(excalidrawAPI?.getSceneElements())}
-                >
-                  Save Sketch
-                </button>
+                {!readOnly && (
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      width: "100%", // Ensure the parent div has enough width to distribute space
+                      gap: "10px", // Optional: add gap for consistent spacing
+                    }}
+                  >
+                    <button
+                      style={{
+                        padding: "2px",
+                        borderRadius: "5px",
+                        background: "#70b1ec",
+                        border: "none",
+                        color: "#fff",
+                        height: "45px",
+
+                        width: "45px",
+                        fontWeight: "bold",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                      onClick={() => navigate("/")}
+                    >
+                      <ArrowBackIcon />
+                    </button>
+                    <button
+                      style={{
+                        padding: "6px",
+                        borderRadius: "5px",
+                        background: "#70b1ec",
+                        border: "none",
+                        width: "45px",
+                        height: "45px",
+
+                        color: "#fff",
+                        fontWeight: "bold",
+                      }}
+                      onClick={shareScenesData}
+                    >
+                      <ShareIcon />
+                    </button>
+                    <button
+                      style={{
+                        padding: "2px",
+                        borderRadius: "5px",
+                        background: "#70b1ec",
+                        border: "none",
+                        color: "#fff",
+                        width: "100px",
+                        height: "45px",
+                        fontWeight: "bold",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                      onClick={() =>
+                        updateData(excalidrawAPI?.getSceneElements())
+                      }
+                    >
+                      Save Sketch
+                    </button>
+                  </div>
+                )}
               </>
             );
           }}
